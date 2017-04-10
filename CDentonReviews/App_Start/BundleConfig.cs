a635498712c34f6d36ce577fa3f79f4b94242b28
Cameron Denton <cdentonPD@gmail.com>
2017-04-10 02:20:29 -0400

    AllModsAdded

--- CDentonReviews/App_Start/BundleConfig.cs
+++ CDentonReviews/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace CDentonReviews
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-darkly.css",
                       "~/Content/site.css"));
         }
     }
